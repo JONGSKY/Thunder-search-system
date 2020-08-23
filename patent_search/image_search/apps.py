d34081836201e346b31d90db98a698882a5795f8
JONGSKY <ljhljh0125@gmail.com>
2020-08-24 03:42:44 +0900

    django docker화에 따른 patent_search 폴더 삭제

--- patent_search/image_search/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ImageSearchConfig(AppConfig):
-    name = 'image_search'
--- patent_search/main_page/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MainPageConfig(AppConfig):
-    name = 'main_page'
