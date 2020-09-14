b3d83d404a90ec6b46192fd0808a13404c64f798
quartzC <68604786+quartzC@users.noreply.github.com>
2020-09-14 19:10:58 +0900

    PS #60 - Combining the new template with the existing front-end (#61)
    * 새로운 bootstrap에 맞게 front-end 작업 완료
    
    * 상단 메뉴바 수정 완료
    
    * 상단 메뉴바 글꼴 및 기능 수정, 모바일 이슈 해결, footer 이메일 제거 완료
    
    * 상단 home 버튼 글씨 굵기 수정 완료, text-search -> patent-search로 수정 완료
    
    Co-authored-by: quartz <quartz12@gmail.com>

--- app/patent_search/views.py
+++ app/patent_search/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request, 'patent_search/home.html')
+    return render(request, 'patent_search/index.html')
