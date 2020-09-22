b96c114ab1c00924fa3eafeefd6c3941295dc5cd
quartz <quartz12@gmail.com>
2020-09-23 02:53:09 +0900

    header 규격 전면 통일 및 patent_search url과 patent_search_result url 분리 완료

--- app/patent_search/urls.py
+++ app/patent_search/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='patent_search_home'),
+    path('result', views.search_action, name='search'),
 ]
--- app/patent_search/views.py
+++ app/patent_search/views.py
@@ -3,4 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request, 'patent_search/index.html')
+    return render(request, 'patent_search/patent_search.html')
+
+
+def search_action(request):
+    search_input = request.GET.get('search_name', "") 
+    return render(request, 'patent_search/patent_search_result.html', { 'search_name' : search_input})
+    
+
