2120b29a31dd29c279a99398390ff74846b3d5f3
sangwoo <psu8430@gmail.com>
2020-09-30 00:09:37 +0900

    change url setting in Trademark Search

--- app/trademark_search/urls.py
+++ app/trademark_search/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='logo_search'),
+    path('logo_search/', views.logo_search, name='logo_search'),
+    path('vienna_search/', views.vienna_search, name='vienna_search'),
+    path('logo_search/result', views.logo_search_result, name='logo_search_result'),
+    path('vienna_search/result', views.vienna_search_result, name='vienna_search_result')
 ]
--- app/trademark_search/views.py
+++ app/trademark_search/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+def logo_search(request):
+    return render(request, 'trademark_search/logo_search.html')
 
-def home(request):
-    return render(request, 'trademark_search/home.html')
+
+def vienna_search(request):
+    return render(request, 'trademark_search/vienna_search.html')
+
+@csrf_exempt
+def logo_search_result(request):
+    logo_search_image = request.FILES["logo_search_image"]
+    return render(request, 'trademark_search/logo_search_result.html')
+
+@csrf_exempt
+def vienna_search_result(request):
+    vienna_search_image = request.FILES["vienna_search_image"]
+    return render(request, 'trademark_search/vienna_search_result.html')
