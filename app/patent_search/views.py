58a41bf78ba981261647b7fc5ee750803111896a
quartz <quartz12@gmail.com>
2020-10-07 15:57:42 +0900

    실제 데이터 화면에 출력 완료, modal창과 출력된 데이터 세부 수정 완료

--- app/patent_search/views.py
+++ app/patent_search/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.views.generic  import ListView
 from elasticsearch_dsl.query import Q
 from .documents import PatentDocument, PatentEmbeddingDocument, PatentNgramDocument
+from django.core.paginator import Paginator
 # Create your views here.
 
 def patent_search(request):
@@ -9,37 +11,29 @@ def patent_search(request):
 
 def patent_search_result(request):
     search_input = request.GET.get('search_name', "") 
-    result = {'search_name' : search_input}
-
-    # s = PatentDocument.search().query('match', patent_id='10000001')
-    # s = PatentDocument.search().query('match_phrase', abstract='supporting')
-    s = PatentDocument.search().query(
-                Q('match_phrase', abstract='supporting') & 
-                Q('match_phrase', abstract='plate')
-            )
-
-    for test in s:
-        print(test)
-        print(
-            "patent_title : {}, abstract {}".format(test.patent_id, test.abstract)
-        )
-
-    p = PatentEmbeddingDocument.search().query('match', patent_id='10000001')
-
-    for hit in p:
-        print(hit)
-        print(
-            "patent_id : {}, embedding {}".format(hit.patent_id, hit.embedding)
-        )
-
-    p = PatentNgramDocument.search().query('match', patent_id='10000001')
-
-    for hit in p:
-        print(hit)
-        print(
-            "patent_id : {}, ngram_words {}".format(hit.patent_id, hit.ngram_words)
-        )
-
-    return render(request, 'patent_search/patent_search_result.html', result)
     
+    patent_list = PatentDocument.search().filter('match', abstract=search_input)
+    
+    paginator = Paginator(list(patent_list), 8)
+    page = int(request.GET.get('page', 1))
+    patent_list = paginator.get_page(page)
+
+    page_numbers_range = 5
+    
+    max_index = len(paginator.page_range)
+    current_page = page
+    start_index = int((current_page - 1) / page_numbers_range) * page_numbers_range
+    end_index = start_index + page_numbers_range
+    
+    if end_index >= max_index:
+        end_index = max_index
+    paginator_range = paginator.page_range[start_index:end_index]
+
+    search_result = {'search_name' : search_input,
+                     'patent_list' : patent_list,
+                     'page' : page,
+                     'paginator_range' : paginator_range
+                     }
+    
+    return render(request, 'patent_search/patent_search_result.html', search_result)
 
