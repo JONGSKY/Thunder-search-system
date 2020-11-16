566da749416383afebb7f2357c3a65d431f39065
quartz <quartz12@gmail.com>
2020-11-16 22:29:12 +0900

    elasticsearch에서 DB 출력으로 변경

--- app/patent_search/views.py
+++ app/patent_search/views.py
@@ -2,19 +2,27 @@ from django.shortcuts import render
 from .documents import PatentDocument
 from django.core.paginator import Paginator
 from django.contrib import messages
+from patent_search.models import Patentsview
+from django.db.models import Q
+import json
 
-
-# Create your views here.
+with open('patent_word.txt') as word_file:
+    word_data = json.load(word_file)
+search_auto_complete = word_data['patent_word']
 
 def patent_search(request):
-    return render(request, 'patent_search/patent_search.html')
+
+    result = {'search_auto_complete' : search_auto_complete}
+    
+    return render(request, 'patent_search/patent_search.html', result)
 
 
 def patent_search_result(request):
     search_input = request.GET.get('search_name', "")
     
-    patent = list(PatentDocument.search().filter('match', abstract=search_input))
-    
+    ##patent = list(PatentDocument.search().filter('match', abstract=search_input))
+    patent = Patentsview.objects.filter(Q(abstract__contains=search_input)).order_by('date')
+
     if len(patent) != 0:
 
         paginator = Paginator(patent, 8)
@@ -23,7 +31,7 @@ def patent_search_result(request):
         
         patent_list = paginator.page(page)
         
-        page_numbers_range = 5
+        page_numbers_range = 9
         
         max_index = len(paginator.page_range)
         current_page = int(page)
@@ -49,7 +57,3 @@ def patent_search_result(request):
         search_result = {'search_name' : search_input,
                         }
         return render(request, 'patent_search/patent_search_none_result.html', search_result)
- 
-
-
-
